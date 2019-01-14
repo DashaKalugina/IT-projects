65384b0c9f69f4752a510726c9e6841431893e9e
DashaKalugina <dashakalugina14@yandex.ru>
2019-01-14 14:21:50 +0300

    Телефонный справочник

---
+++ TelephoneBookC#/TelephoneBookTests/TelephoneBook/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelephoneBook
+{
+    public class Record
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Phone)}: {Phone}";
+        }
+    }
+    public abstract class Command
+    {
+        protected readonly CommandLoop _loop;
+        protected Command(CommandLoop loop)
+        {
+            _loop = loop;
+        }
+        public abstract void Do(string commandArgs);
+        public abstract string Name { get; }
+
+        protected string RemovePrefix(string line)
+        {
+            Debug.Assert(line.StartsWith(Name, StringComparison.CurrentCultureIgnoreCase));
+
+            if (line.Length > Name.Length)
+            {
+                return line.Substring(Name.Length + 1);
+            }
+            return "";
+        }
+    }
+
+    public  abstract class CommandLoop
+    {
+        public bool Stop { get; set; }
+
+        public List<Record> Records { get; set; } = new List<Record>();
+
+        protected readonly CommandFactory _commandFactory;
+
+        public CommandLoop()
+        {
+            _commandFactory = new CommandFactory(this);
+        }
+
+        public abstract void DoLoop();
+
+        public abstract void WriteLine(string line);
+
+        public abstract void ReadKey();
+
+
+    }
+    public class ConsoleCommandLoop : CommandLoop
+    {
+
+        public override void DoLoop()
+        {
+            while (!Stop)
+            {
+                var line = Console.ReadLine();
+                var command = _commandFactory.GetCommand(line);
+
+                command.Do(line);
+            }
+        }
+
+        public override void WriteLine(string line)
+        {
+            Console.WriteLine(line);
+        }
+
+        public override void ReadKey()
+        {
+            Console.ReadKey();
+        }
+    }
+    public class CommandFactory
+    {
+        private readonly CommandLoop _loop;
+        public CommandFactory(CommandLoop loop)
+        {
+            _loop = loop;
+        }
+        public Command GetCommand(string line)
+        {
+            if (line.StartsWith("Выход", StringComparison.CurrentCultureIgnoreCase))
+                return new ExitCommand(_loop);
+
+            if (line.StartsWith("Добавить", StringComparison.CurrentCultureIgnoreCase))
+                return new AddCommand(_loop);
+
+            if (line.StartsWith("Найти", StringComparison.CurrentCultureIgnoreCase))
+                return new SearchCommand(_loop);
+
+            if (line.StartsWith("Удалить", StringComparison.CurrentCultureIgnoreCase))
+                return new DeleteCommand(_loop);
+
+            return new UnknownCommand(_loop);
+        }
+    }
+    public class DeleteCommand : Command
+    {
+        public DeleteCommand(CommandLoop loop) : base(loop)
+        { }
+        
+        public override void Do(string commandargs)
+        {
+            var arg = RemovePrefix(commandargs);
+            var countDeleted = 0;
+            var phoneLength = arg.IndexOf(" ");
+            if (phoneLength == -1)
+            {
+                var searchResult = _loop.Records.Where(r => r.Name.Equals(arg, StringComparison.CurrentCultureIgnoreCase) || r.Phone.Equals(arg, StringComparison.CurrentCultureIgnoreCase));
+                foreach (var record in searchResult.ToArray())
+                {
+                    Console.WriteLine("Удалена запись \"" + record.ToString() + "\"");
+                    _loop.Records.Remove(record);
+                    countDeleted++;
+                }
+            }
+            else
+            {
+                var phone = arg.Substring(0, phoneLength);
+                var name = arg.Substring(phoneLength + 1);
+                var searchResult = _loop.Records.Where(r => r.Name.Equals(name, StringComparison.CurrentCultureIgnoreCase) && r.Phone.Equals(phone, StringComparison.CurrentCultureIgnoreCase));
+                foreach (var record in searchResult.ToArray())
+                {
+                    Console.WriteLine("Удалена запись \"" + record.ToString() + "\"");
+                    _loop.Records.Remove(record);
+                    countDeleted++;
+                }
+            }
+            if (countDeleted == 0)
+            {
+                _loop.WriteLine("Не найдено записей для удаления");
+            }
+            else
+                _loop.WriteLine("Всего удалено записей из справочника: " + countDeleted);
+        }
+
+        public override string Name => "Удалить";
+    }
+
+    public class SearchCommand : Command
+    {
+        public SearchCommand(CommandLoop loop) : base(loop)
+        {
+        }
+
+        public override void Do(string commandargs)
+        {
+            bool somethingfound = false;
+            bool f = false;
+            var arg = RemovePrefix(commandargs);
+            var phoneLength = arg.IndexOf(" ");
+            if (phoneLength == -1)
+            {
+                var searchResult = _loop.Records.Where(r => r.Name.Contains(arg) || r.Phone.Contains(arg));
+                foreach (var record in searchResult)
+                {
+                    somethingfound = true;
+                    if (!f)
+                    {
+                        Console.WriteLine("Найденные записи по запросу \"" + arg + "\":");
+                        f = true;
+                    }
+                    _loop.WriteLine(record.ToString());
+                }
+            }
+            else
+            {
+                var phone = arg.Substring(0, phoneLength);
+                var name = arg.Substring(phoneLength + 1);
+                var searchResult = _loop.Records.Where(r => r.Name.Contains(name) || r.Phone.Contains(phone));
+                foreach (var record in searchResult)
+                {
+                    somethingfound = true;
+                    if (!f)
+                    {
+                        Console.WriteLine("Найденные записи по запросу \"" + arg + "\":");
+                        f = true;
+                    }
+                    _loop.WriteLine(record.ToString());
+                }
+            }
+            if (!somethingfound)
+                _loop.WriteLine("Ничего не найдено");
+        }
+
+        public override string Name { get { return "Найти"; } }
+    }
+
+    public class AddCommand : Command
+    {
+        public AddCommand(CommandLoop loop) : base(loop)
+        {
+        }
+        
+        public override void Do(string commandargs)
+        {
+            var arg = RemovePrefix(commandargs);
+            var phoneLength = arg.IndexOf(" ");
+
+            if (phoneLength == -1 || phoneLength == arg.Length + 1)
+            {
+                Console.WriteLine("Чтобы добавить контакт, введите номер телефона и имя абонента!");
+            }
+            else
+            {
+                var phone = arg.Substring(0, phoneLength);
+                var name = arg.Substring(phoneLength + 1);
+                _loop.Records.Add(new Record
+                {
+                    Name = name,
+                    Phone = phone
+                });
+            }
+        }
+
+        public override string Name => "Добавить";
+    }
+
+    public class UnknownCommand : Command
+    {
+        public UnknownCommand(CommandLoop loop) : base(loop)
+        { }
+
+        public override void Do(string commandargs)
+        {
+            _loop.WriteLine("Команда непонятна, попробуйте еще раз");
+        }
+
+        public override string Name { get { throw new NotSupportedException(); } }
+    }
+
+    public class ExitCommand : Command
+    {
+        public ExitCommand(CommandLoop loop) : base(loop)
+        { }
+        public override void Do(string commandArgs)
+        {
+            _loop.Stop = true;
+            _loop.WriteLine("Для завершения программы нажмите любую клавишу");
+            _loop.ReadKey();
+        }
+
+        public override string Name { get { throw new NotSupportedException(); } }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Калугина Дарья, гр. РИ-250004. Телефонный справочник.\nПрограмма принимает команды в следующем формате:\nДобавить Телефон ФИО\nНайти Текст для поиска\nУдалить Телефон/ФИО\nВыход");
+            new ConsoleCommandLoop().DoLoop();
+        }
+    }
+}
---
+++ TelephoneBookC#/TelephoneBookTests/TelephoneBookTest/Class1.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelephoneBook;
+
+namespace TelephoneBookTest
+{
+    [TestFixture]
+    public class CommandTests
+    {
+        [Test]
+        public void CreateCommandFactoryTest()
+        {
+            new CommandFactory(new TestCommandLoop());
+        }
+
+        [Test]
+        public void AddContactTest()
+        {
+            ObjectMother.Reset();
+            ObjectMother.CommandFactory.GetCommand("Добавить 03 Скорая").Do("Добавить 03 Скорая");
+            Assert.That(ObjectMother.Loop.Records.Any(r => r.Name == "Скорая"));
+            Assert.That(ObjectMother.Loop.Records.Any(r => r.Phone == "03"));
+        }
+        [Test]
+        public void AddAndSearchContactTest()
+        {
+            ObjectMother.Reset();
+            ObjectMother.CommandFactory.GetCommand("добавить 9090287694 Бабушка").Do("добавить 9090287694 Бабушка");
+            ObjectMother.CommandFactory.GetCommand("Найти Бабушка").Do("Найти Бабушка");
+            Assert.That(ObjectMother.Loop.Lines.Any(l => l.Contains("Бабушка")));
+        }
+        [Test]
+        public void AddAndRemoveContactTest()
+        {
+            ObjectMother.Reset();
+            ObjectMother.CommandFactory.GetCommand("Добавить 3434567859 Ростелеком").Do("Добавить 3434567859 Ростелеком");
+            ObjectMother.CommandFactory.GetCommand("Удалить Ростелеком").Do("Удалить Ростелеком");
+            Assert.That(ObjectMother.Loop.Lines.All(l => !l.Contains("Ростелеком")));
+
+            ObjectMother.CommandFactory.GetCommand("Добавить 03 Скорая").Do("Добавить 03 Скорая");
+            ObjectMother.CommandFactory.GetCommand("Удалить 03 Скорая").Do("Удалить 03 Скорая");
+            Assert.That(ObjectMother.Loop.Lines.All(l => !l.Contains("Скорая")));
+        }
+
+        [Test]
+        public void UnknownCommandTest()
+        {
+            ObjectMother.Reset();
+            ObjectMother.CommandFactory.GetCommand("fhfjh48494").Do("fhfjh48494");
+            Assert.That(ObjectMother.Loop.Lines.Any());
+        }
+
+        [Test]
+        public void ExitFromProgramTest()
+        {
+            ObjectMother.Reset();
+            ObjectMother.CommandFactory.GetCommand("Выход").Do("Выход");
+            Assert.That(ObjectMother.Loop.KeyIsAwaited);
+        }
+        
+    }
+
+    class ObjectMother
+    {
+        private static CommandFactory _factory;
+        private static TestCommandLoop _loop;
+
+        public static TestCommandLoop Loop => _loop;
+
+        static ObjectMother()
+        {
+            Reset();
+        }
+
+        public static void Reset()
+        {
+            _loop = new TestCommandLoop();
+            _factory = new CommandFactory(_loop);
+        }
+
+        public static CommandFactory CommandFactory => _factory;
+    }
+
+    class TestCommandLoop : CommandLoop
+    {
+        public override void DoLoop()
+        {
+            throw new NotSupportedException("Вызов цикла из тестов не предусмотрен");
+        }
+
+        public bool KeyIsAwaited { get; set; }
+
+        public override void ReadKey()
+        {
+            KeyIsAwaited = true;
+        }
+
+        public List<string> Lines { get; } = new List<string>();
+
+        public override void WriteLine(string line)
+        {
+            Lines.Add(line);
+        }
+    }
+}
